cab489ae6565b0ef61014a96c24b067cd208ee60
emilien3 <emilien.sanchez16@gmail.com>
2024-02-29 19:11:49 +0100

    implementation de l'algo terminé

--- src/modele/TriInsertion.java
+++ src/modele/TriInsertion.java
@@ -5,42 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TriInsertion {
-	
-	public static void traitement (char [] anagramme, int taille_anagramme) {
-        /* boucle de traitement */
-		for (int i = 0; i < taille_anagramme-1; i++) {
-			char car = anagramme [i];
+
+	public void traitement(char[] anagramme, int taille_anagramme) {
+		for (int i = 0; i < taille_anagramme - 1; i++) {
+			char car = anagramme[i];
 			int j = i;
-			while ((j>0) && (car< anagramme[j-1])) {
-				anagramme [j] = anagramme [j-1];
-				j = -1;
+			while ((j > 0) && (car < anagramme[j - 1])) {
+				anagramme[j] = anagramme[j - 1];
+				j--;
 			}
-            anagramme[j] = car ;
-         }
+			anagramme[j] = car;
+		}
 	}
 
- 	public static void anagramme() {
+	public String lecture() throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
-	    BufferedReader br = new BufferedReader(isr);
+		BufferedReader br = new BufferedReader(isr);
+		String text = br.readLine();
+		return text;
+	}
+
+	public void anagrammeLu() {
+		String text;
 		try {
-			String text = br.readLine();
-			System.out.println(text);
-			int taille = text.length();
-			int taille_anagramme = 0;
-			char[] anagramme = text.toCharArray();
-			while (taille > taille_anagramme) {
-				traitement(anagramme,taille_anagramme);
-				taille_anagramme++;
-			}
+			text = lecture();
+			anagramme(text);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
- 	
- 	
- 	public static void main(String[] args) {
- 		anagramme();
- 	}
- 	
- 	
+
+	public void anagramme(String text) {
+		System.out.println(text);
+		int taille_anagramme = text.length();
+		char[] anagramme = text.toCharArray();
+		traitement(anagramme, taille_anagramme);
+		System.out.println(anagramme);
+
+	}
+
+	public static void main(String[] args) {
+		TriInsertion t = new TriInsertion();
+		t.anagrammeLu();
+	}
+
 }
